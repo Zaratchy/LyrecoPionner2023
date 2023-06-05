59e37dca1554b55eaf5b6d0138b015464e41c7d5
Josic <libert.josic@gmail.com>
2023-06-05 22:38:02 +0200

    first commit

---
+++ src/main/java/com/example/LyrecoPionner2023/LyrecoPionner2023Application.java
@@ -0,0 +1,13 @@
+package com.example.LyrecoPionner2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyrecoPionner2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyrecoPionner2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LyrecoPionner2023/LyrecoPionner2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LyrecoPionner2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyrecoPionner2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
