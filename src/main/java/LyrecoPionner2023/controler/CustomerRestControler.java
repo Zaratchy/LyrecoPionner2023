988839b33f7205c2be06ffac0b926aede30dff9c
Josic <libert.josic@gmail.com>
2023-06-05 23:29:35 +0200

    add customer entity

---
+++ src/main/java/LyrecoPionner2023/controler/CustomerRestControler.java
@@ -0,0 +1,13 @@
+package LyrecoPionner2023.controler;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+public class CustomerRestControler {
+
+
+
+}
+
---
+++ src/main/java/LyrecoPionner2023/model/Customer.java
@@ -0,0 +1,38 @@
+package LyrecoPionner2023.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "CUSTOMER")
+
+public class Customer implements Serializable {
+
+    /**
+
+     Identifiant de serialisation*/
+    private static final long serialVersionUID = 1814504864568721993L;
+
+    @Id
+    @GeneratedValue(generator = "SEQ_CUSTOMER_ID", strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private int phone;
+    private String email;
+    private String civility;
+    @ManyToOne
+    private Credential credential;
+
+
+}
+
