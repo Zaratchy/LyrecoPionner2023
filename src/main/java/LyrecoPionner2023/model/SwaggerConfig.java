c8e9c408e3702f559c43a10a07b2ec3bdf54161f
Josic <libert.josic@gmail.com>
2023-06-08 23:42:40 +0200

    feature/customer: pom.xml dependencies + swagger config

---
+++ src/main/java/LyrecoPionner2023/model/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package LyrecoPionner2023.model;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket atividadeApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.atividades.apirest"))
+                .paths(regex("/api.*"))
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+
+        ApiInfo apiInfo = new ApiInfo(
+                "Atividades API REST",
+                "API REST de cadastro de atividades.",
+                "1.0",
+                "Terms of Service",
+                new Contact("João VR", "www.una.br/",
+                        " "),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>()
+        );
+
+        return apiInfo;
+    }
+}
